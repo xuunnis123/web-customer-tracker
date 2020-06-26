1553a33c02425e8472b83760d382022c4b2a0cad
jiyanglin <jiyanglin@localhost>
2020-06-26 15:43:24 +0800

    Add some page and controller

---
+++ src/com/ezra/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.ezra.springdemo.dao;
+
+import java.util.List;
+
+import com.ezra.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
